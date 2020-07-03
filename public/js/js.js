3827ce5675016ead7c760852802c1cc932abcd26
error401de <error401blog@gmail.com>
2020-07-03 15:18:55 +0200

    render participants from websocket message

--- public/js/js.js
+++ public/js/js.js
@@ -14,8 +14,18 @@ function renderBox({id, name}) {
 	document.getElementById('box-maps').appendChild(div);
 }
 
-function handleParticipants() {
-	console.log("hiiiiii");
+function renderUser({name}) {
+	const div = document.createElement('div');
+	div.setAttribute('class', 'user');
+	div.setAttribute('id', 'user'+ name);
+	div.textContent = "User " + name;
+	document.getElementById('box-participants').appendChild(div);
+}
+
+function handleParticipants(data) {
+	const clearList = document.getElementById('box-participants');
+	clearList.textContent = '';
+	data.items.forEach(renderUser);
 }
 
 function handleResult() {
@@ -29,13 +39,14 @@ function handleReset() {
 window.onload=function(){
 	createMapBoxes();
 	const ws = new WebSocket('ws://' + document.location.host);
-	
+
 	ws.onmessage=function(message){
 		const json = JSON.parse(message.data);
-		console.log(json);
+	ws.send(JSON.stringify(["vetoed", {maps: ['de_dust2']}])) // only for dev reasons
+
 		switch (json[0]) {
 			case 'participants':
-				handleParticipants();
+				handleParticipants(json[1]);
 				break;
 			case 'result':
 				handleResult();
