12473c3e5ef9032ef0f5f8d2158ee5ead2f658e2
error401de <error401blog@gmail.com>
2020-07-03 14:18:51 +0200

    preparation further functions

--- public/js/js.js
+++ public/js/js.js
@@ -14,6 +14,37 @@ function renderBox({id, name}) {
 	document.getElementById('box-maps').appendChild(div);
 }
 
+function handleParticipants() {
+	console.log("hiiiiii");
+}
+
+function handleResult() {
+	console.log("hiiiiii");
+}
+
+function handleReset() {
+	console.log("hiiiiii");
+}
+
 window.onload=function(){
 	createMapBoxes();
+	const ws = new WebSocket('ws://' + document.location.host);
+	
+	ws.onmessage=function(message){
+		const json = JSON.parse(message.data);
+		console.log(json);
+		switch (json[0]) {
+			case 'participants':
+				handleParticipants();
+				break;
+			case 'result':
+				handleResult();
+				break;
+			case 'reset':
+				handleReset();
+				break;
+		default:
+			console.log('Message not handled');
+		}
+	}
 }
