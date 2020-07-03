88593c5da3802b40d0b38666c292fc52045d93d4
error401de <error401blog@gmail.com>
2020-07-03 13:29:14 +0200

    map box dynamic creation, map json fix

---
+++ public/js/js.js
@@ -0,0 +1,18 @@
+async function createMapBoxes() {
+
+	let url = 'config/maps_competitive.json';
+	let response = await fetch(url);
+	let json = await response.json();
+
+	for (var i = 0; i < json.items.length; i++) {
+		renderBox(json.items[i].id, json.items[i].name)
+	}
+}
+
+function renderBox(id, text) {
+	var div = document.createElement('div');
+	div.setAttribute('class', 'map');
+	div.setAttribute('id', id);
+	div.textContent = text;
+	document.getElementById('box-maps').appendChild(div);
+}
