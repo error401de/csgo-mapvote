4bd5ed9d8e1062b0c33bb5846550faf6a1e0de75
acra5y <oli.schmelzle@gmail.com>
2020-07-05 14:06:33 +0200

    Add cache headers for images

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const config = require('./config.json')
+const mime = require('mime-types');
 
+const config = require('./config.json');
 const handleWebsockets = require('./base/handleWebsockets');
+
 const app = express();
 
 const wss = require('express-ws')(app);
 
-app.use(express.static('public'));
+app.use(express.static('public', {
+	setHeaders: (res, path) => {
+		switch (mime.lookup(path)) {
+			case 'image/png':
+			case 'image/svg+xml':
+				res.setHeader('Cache-Control', 'public, max-age=86400');
+				break;
+		}
+	}
+}));
+
 
 app.ws(config.webSocketBasePath, handleWebsockets(wss));
 
