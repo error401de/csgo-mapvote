1da2f70db048bab0f94e7a7fa782274230a9804c
error401de <error401blog@gmail.com>
2020-07-09 17:12:38 +0200

    force integers as slider values

--- public/js/js.js
+++ public/js/js.js
@@ -143,9 +143,9 @@
 
 			currentSlider.onchange = function () {
 				if (slider.id === 'slider-votes') {
-					items[0].votesPerParticipant = currentSlider.value;
+					items[0].votesPerParticipant = parseint(currentSlider.value);
 				} else if (slider.id === 'slider-vetoes') {
-					items[0].vetoesPerParticipant = currentSlider.value;
+					items[0].vetoesPerParticipant = parseint(currentSlider.value);
 				}
 				ws.send(JSON.stringify(['slider', { items }]));
 			}
