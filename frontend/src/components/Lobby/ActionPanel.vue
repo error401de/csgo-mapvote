ea69687f38e47a1a793370e7e3e57c90e580a87f
acra5y <oli.schmelzle@gmail.com>
2020-09-15 17:52:43 +0200

    Add reset button

--- frontend/src/components/Lobby/ActionPanel.vue
+++ frontend/src/components/Lobby/ActionPanel.vue
@@ -1,20 +1,28 @@
 <template>
   <Panel headline="Action Menu">
     <div>
-      <ShowResultButton />
+      <MessageSendingButton displayText="Show Result" :messageType="messageTypeShowResult" />
+      <MessageSendingButton displayText="Reset All Choices" :messageType="messageTypeReset" />
     </div>
   </Panel>
 </template>
 
 <script>
 import Panel from "@/components/Layout/Panel.vue";
-import ShowResultButton from "@/components/Lobby/ShowResultButton.vue";
+import MessageSendingButton from "@/components/Lobby/MessageSendingButton.vue";
+import { CLIENT_MESSAGES } from "common/messageTypes";
 
 export default {
   name: "ActionPanel",
   components: {
+    MessageSendingButton,
     Panel,
-    ShowResultButton,
+  },
+  data() {
+    return {
+      messageTypeReset: CLIENT_MESSAGES.RESET,
+      messageTypeShowResult: CLIENT_MESSAGES.SHOW_RESULT,
+    };
   },
 };
 </script>
---
+++ frontend/src/components/Lobby/MessageSendingButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <Button :msg="displayText" @buttonClick="sendMessage" />
+</template>
+
+<script>
+import Button from "@/components/Button.vue";
+
+export default {
+  name: "MessageSendingButton",
+  components: {
+    Button,
+  },
+  props: {
+    messageType: String,
+    displayText: String,
+  },
+  methods: {
+    sendMessage() {
+      this.$socket.sendObj([this.messageType]);
+    },
+  },
+};
+</script>
